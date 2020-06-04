9a5b3f1684b13a57684d4c4777a2db215927602f
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-06-04 21:13:57 +0300

    fixed errors and added tests

--- __tests__/404.test.js
+++ __tests__/404.test.js
@@ -1,7 +1,7 @@
 'use strict';
 const {server} = require('../lib/server.js');
-const supergoose = require('@code-fellows/supergoose');
-const mockRequest = supergoose(server);
+const supertest = require('supertest');
+const mockRequest = supertest(server);
 
 describe('404 internal server error Middleware', () => {
 
